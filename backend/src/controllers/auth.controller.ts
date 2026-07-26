1e915972624a43363aa42f4eac904e7682a7ce80
thejovamsi <thejovamsi9581300609@gmail.com>
2026-07-26 01:11:14 -0400

    User existence check with Email

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -2,6 +2,18 @@ import { registerUser } from "../services/auth.service.js";
 import type { Request, Response } from "express";
 
 export const register = async (req: Request, res: Response) => {
-    const user = await registerUser(req.body);
-    res.status(201).json(user);
+    try {
+        const user = await registerUser(req.body);
+        res.status(201).json(user);
+    } catch (error) {
+        if (error instanceof Error && error.message === "User with this email already exists") {
+            return res.status(409).json({
+                message: error.message
+            });
+        } else {
+            return res.status(500).json({
+                message: "Internal server error"
+             });
+        }
+    }
 };
--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -1,8 +1,10 @@
 import prisma from "../config/prisma.js";
+import bcrypt from "bcrypt";
+
 type RegisterUserData = {
     name: string;
     email: string;
-    passwordHash: string;
+    password: string;
     phoneNumber?: string;
     createdAt?: Date;
 };
@@ -10,7 +12,17 @@ type RegisterUserData = {
 
 
 export const registerUser = async (userData: RegisterUserData) => {
-    const { name, email, passwordHash, phoneNumber } = userData;
+    const { name, email, password, phoneNumber } = userData;
+    const existingUser = await prisma.user.findUnique({
+        where: {
+            email
+        }
+    });
+    if (existingUser) {
+        throw new Error("User with this email already exists");
+    }
+
+    const passwordHash = await bcrypt.hash(password,10);
     const user = await prisma.user.create({
         data: {
             name,
@@ -22,7 +34,8 @@ export const registerUser = async (userData: RegisterUserData) => {
             name:true,
             email:true,
             phoneNumber:true,
-            createdAt:true
+            createdAt:true,
+            passwordHash:true
         }
     });
     return user;
